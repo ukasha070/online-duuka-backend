f81c0d4c1c0474eda7e6f08f9e2d38d82dbb0170
ukasha katuntu <81513697+ukasha070@users.noreply.github.com>
2026-06-20 13:19:06 +0300

    refactor: add custom orjson response

---
+++ app/core/_orjson.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+import orjson
+from fastapi import Response
+
+
+class CustomORJSONResponse(Response):
+    media_type = "application/json"
+
+    def render(self, content: Any) -> bytes:
+        assert orjson is not None, "orjson must be installed"
+        return orjson.dumps(content, option=orjson.OPT_INDENT_2)
